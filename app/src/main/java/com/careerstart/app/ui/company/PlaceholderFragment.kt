58da45df0fea25c68426bf8d598dd3a77d4834ec
Yasir Ali <ali.yasir0@gmail.com>
2019-12-09 18:03:45 +0500

    Add projects in Company Screen

--- app/src/main/java/com/careerstart/app/ui/company/PlaceholderFragment.kt
+++ app/src/main/java/com/careerstart/app/ui/company/PlaceholderFragment.kt
@@ -29,10 +29,6 @@ class PlaceholderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
         return root
     }
 
